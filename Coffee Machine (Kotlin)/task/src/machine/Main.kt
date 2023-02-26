3297a4ff278129ece6c9dc2f31de02354539918e
shubham <thoratshubham161@gmail.com>
2023-02-26 15:58:48 +0530

    coffee machine

---
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -0,0 +1,11 @@
+package machine
+
+fun main() {
+    println("Starting to make a coffee\n" +
+            "Grinding coffee beans\n" +
+            "Boiling water\n" +
+            "Mixing boiled water with crushed coffee beans\n" +
+            "Pouring coffee into the cup\n" +
+            "Pouring some milk into the cup\n" +
+            "Coffee is ready!")
+}
