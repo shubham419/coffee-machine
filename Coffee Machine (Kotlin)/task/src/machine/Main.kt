cf7e98fac64b0d8053cab1cf691edd478d2e3034
shubham <thoratshubham161@gmail.com>
2023-02-27 09:18:48 +0530

    stage 3

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,14 +1,29 @@
 package machine
 
-fun main() {
-    println("Write how many cups of coffee you will need:")
-    val noOfCoffee = readln().toInt()
-    val requiredWater = noOfCoffee * 200
-    val requiredMilk = noOfCoffee * 50
-    val requiredBeans = noOfCoffee * 15
 
-    println("For $noOfCoffee cups of coffee you will need:\n" +
-            "$requiredWater ml of water\n" +
-            "$requiredMilk ml of milk\n" +
-            "$requiredBeans g of coffee beans")
+class CoffeeMachine(waterAmount: Int, milkAmount: Int, beansAmount: Int) {
+    private val totalCups = minOf((waterAmount / 200) as Int, minOf(milkAmount / 50 as Int, beansAmount / 15 as Int))
+    fun getCoffee(cups: Int) {
+        val cups = totalCups - cups
+        when {
+            cups == 0 -> println("Yes, I can make that amount of coffee")
+            cups > 0 -> println("Yes, I can make that amount of coffee (and even $cups more than that)")
+            cups < 0 -> println("No, I can make only $totalCups cups of coffee")
+        }
+    }
+}
+
+fun main() {
+    println("Write how many ml of water the coffee machine has:")
+    val waterAmount = readln().toInt()
+    println("Write how many ml of milk the coffee machine has:")
+    val milkAmount = readln().toInt()
+    println("Write how many grams of coffee beans the coffee machine has:")
+    val beansAmount = readln().toInt()
+    val machine = CoffeeMachine(waterAmount, milkAmount, beansAmount)
+    println("Write how many cups of coffee you will need:")
+    val cups = readln().toInt()
+
+    machine.getCoffee(cups)
+
 }
