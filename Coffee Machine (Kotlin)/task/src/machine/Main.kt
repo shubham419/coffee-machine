45dba79990946126a4f045ab01753df34bd702b6
shubham <thoratshubham161@gmail.com>
2023-03-06 11:05:09 +0530

    stage 5

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,5 +1,7 @@
 package machine
 
+import kotlin.system.exitProcess
+
 
 class CoffeeMachine() {
     private var waterAmount = 400
@@ -8,49 +10,50 @@ class CoffeeMachine() {
     private var disposableCups = 9
     private var money = 550
     private var action = ""
-    private val totalCups = minOf((waterAmount / 200) as Int, minOf(milkAmount / 50 as Int, beansAmount / 15 as Int))
-    fun getCoffee(cups: Int) {
-        val cups = totalCups - cups
-        when {
-            cups == 0 -> println("Yes, I can make that amount of coffee")
-            cups > 0 -> println("Yes, I can make that amount of coffee (and even $cups more than that)")
-            cups < 0 -> println("No, I can make only $totalCups cups of coffee")
-        }
-    }
 
-    fun start() {
-        println(
-            "The coffee machine has:\n" + "$waterAmount ml of water\n" + "$milkAmount ml of milk\n" + "$beansAmount g of coffee beans\n" + "$disposableCups disposable cups\n" + "\$$money of money\n"
-        )
+
+    private fun start() {
+        println()
         getAction()
     }
 
     private fun displayData() {
-        println()
+//        println()
         println(
-            "The coffee machine has:\n" + "$waterAmount ml of water\n" + "$milkAmount ml of milk\n" + "$beansAmount g of coffee beans\n" + "$disposableCups disposable cups\n" + "\$$money of money\n"
+            "The coffee machine has:\n" + "$waterAmount ml of water\n" + "$milkAmount ml of milk\n" + "$beansAmount g of coffee beans\n" + "$disposableCups disposable cups\n" + "\$$money of money"
         )
+        start()
     }
 
-    private fun getAction() {
-        println("Write action (buy, fill, take):")
+     fun getAction() {
+        println("Write action (buy, fill, take, remaining, exit):")
         action = readln()
         performAction()
     }
 
 
     private fun performAction() {
+        println()
         when (action) {
             "buy" -> actionBuy()
             "fill" -> actionFill()
             "take" -> actionTake()
+            "remaining" -> displayData()
+            "exit"  -> exit()
         }
     }
 
+    private fun exit() {
+            exitProcess(10)
+    }
+
     private fun actionBuy() {
-        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-        val coffeeType = readln().toInt()
-        when (coffeeType) {
+        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+        val coffeeType = readln()
+        if(coffeeType == "back"){
+            start()
+        }
+        when (coffeeType.toInt()) {
             1 -> buyCoffee(250, 0, 16, 4)
             2 -> buyCoffee(350, 75, 20, 7)
             3 -> buyCoffee(200, 100, 12, 6)
@@ -58,12 +61,29 @@ class CoffeeMachine() {
     }
 
     private fun buyCoffee(requiredWater: Int = 0, requiredMilk: Int = 0, requiredBeans: Int = 0, cost: Int = 0) {
+        if(waterAmount < requiredWater){
+            println("Sorry, not enough water!")
+            start()
+        }
         waterAmount -= requiredWater
+        if(milkAmount < requiredMilk){
+            println("Sorry, not enough milk!")
+            start()
+        }
         milkAmount -= requiredMilk
+        if(beansAmount < requiredBeans){
+            println("Sorry, not enough beans!")
+            start()
+        }
         beansAmount -= requiredBeans
-        money += cost
+        if (disposableCups < 1){
+            println("Sorry, not enough beans!")
+            start()
+        }
         disposableCups--
-        displayData()
+        money += cost
+        println("I have enough resources, making you a coffee!")
+        start()
     }
 
     private fun actionFill() {
@@ -75,17 +95,17 @@ class CoffeeMachine() {
         beansAmount += readln().toInt()
         println("Write how many disposable cups you want to add:")
         disposableCups += readln().toInt()
-        displayData()
+        start()
     }
 
     private fun actionTake() {
         println("I gave you \$$money")
         money = 0
-        displayData()
+        start()
     }
 }
 
 fun main() {
     val machine = CoffeeMachine()
-    machine.start()
+    machine.getAction()
 }
